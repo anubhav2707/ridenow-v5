b5a887cbaaa887f03cfd8e1b03835a4877da9c59
Anubhav Khandelwal <50150129+anubhav2707@users.noreply.github.com>
2026-07-06 18:42:10 +0530

    SCRUM-232: Scaffold the RideNow v5 service repository from scratch

--- apps/api/src/earnings/earnings.module.ts
+++ apps/api/src/earnings/earnings.module.ts
@@ -1,8 +1,7 @@
 import { Module } from "@nestjs/common";
-import { EarningsService } from "./earnings.service";
+import { EarningsController } from "./earnings.controller";
 
 @Module({
-  providers: [EarningsService],
-  exports: [EarningsService],
+  controllers: [EarningsController],
 })
 export class EarningsModule {}
